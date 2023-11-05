4d4ae84e2bfd64ebd5dd43182a6bbf54212b36a9
mdmonir-hossain <monirsoftengr@gmail.com>
2023-11-05 11:31:23 +0600

    router set

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import MainRoutes from './Routes/MainRoutes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={MainRoutes}></RouterProvider>
   </React.StrictMode>,
 )
