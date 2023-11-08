bae1f6e3cfc305c5bdaed9cb7558f07ca29810e6
mdmonir-hossain <monirsoftengr@gmail.com>
2023-11-08 06:39:49 +0600

    error page create

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,9 @@
 
+import Lottie from 'lottie-react';
 import { Helmet } from 'react-helmet';
+import error404 from "../../assets/404.json"
+import { Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
@@ -7,6 +11,18 @@ const ErrorPage = () => {
         <Helmet>
           <title>404 Not Found</title>
         </Helmet>
+        <div className="container mx-auto relative">
+          <Lottie animationData={error404}></Lottie>
+          <Link to='/'>
+            <Button
+              color="gray"
+              className="absolute right-0  left-0 -mt-[300px] text-5xl font-extrabold  "
+            >
+              {" "}
+              Go to Home
+            </Button>
+          </Link>
+        </div>
       </div>
     );
 };
